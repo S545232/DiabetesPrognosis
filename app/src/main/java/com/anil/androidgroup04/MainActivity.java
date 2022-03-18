59f4840c5e0df6ae4455be63968b59ad88084a02
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2022-03-18 14:25:44 -0500

    Main activity, layout created and required buttons added.

--- app/src/main/java/com/anil/androidgroup04/MainActivity.java
+++ app/src/main/java/com/anil/androidgroup04/MainActivity.java
@@ -1,14 +1,28 @@
 package com.anil.androidgroup04;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.cardview.widget.CardView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    CardView diabetesCheckBtn, bmiBtn, medicineAlertsBtn, insulinBtn, pharmacyListBtn, faqBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        getSupportActionBar().setTitle("HOME");
+        
+        diabetesCheckBtn = findViewById(R.id.diabetes_check_btn);
+        bmiBtn = findViewById(R.id.bmi_btn);
+        insulinBtn = findViewById(R.id.insulin_btn);
+        medicineAlertsBtn = findViewById(R.id.medicine_alerts_btn);
+        pharmacyListBtn = findViewById(R.id.pharmacy_near_btn);
+        faqBtn = findViewById(R.id.faq_btn);
     }
 }
