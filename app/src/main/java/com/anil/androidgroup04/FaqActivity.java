42c592e6e9c48ea2660943f2ec19ec1509ea496e
pvinaymadhur <vinaymadhur14@gmail.com>
2022-03-18 20:12:19 -0500

    Faq activity and layout is created

---
+++ app/src/main/java/com/anil/androidgroup04/FaqActivity.java
@@ -0,0 +1,23 @@
+package com.anil.androidgroup04;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FaqActivity extends AppCompatActivity {
+    TextView textlink,txt;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+        textlink = findViewById(R.id.textViewLink);
+        textlink.setMovementMethod(LinkMovementMethod.getInstance());
+        txt=findViewById(R.id.textViLink);
+        txt.setMovementMethod(LinkMovementMethod.getInstance());
+
+    }
+}
