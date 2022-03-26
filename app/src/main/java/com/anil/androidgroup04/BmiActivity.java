94bf4068099ebb5b5f039c8143fc98d32ae2181c
ANIL KOLLA <89354787+S545232@users.noreply.github.com>
2022-03-25 22:39:33 -0500

    BMI views initialized and logic added for the BMI calculation

--- app/src/main/java/com/anil/androidgroup04/BmiActivity.java
+++ app/src/main/java/com/anil/androidgroup04/BmiActivity.java
@@ -1,17 +1,68 @@
 package com.anil.androidgroup04;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class BmiActivity extends AppCompatActivity {
+public class BmiActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText height, weight;
+    Button calculateBtn;
+    TextView bmiTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
 
+        initViews();
+    }
+
+    private void initViews() {
         getSupportActionBar().setTitle("BMI Calculation");
+
+        height = findViewById(R.id.etHeight);
+        weight = findViewById(R.id.etWeight);
+        calculateBtn = findViewById(R.id.btnCalculate);
+        bmiTV = findViewById(R.id.tvResult);
+
+        calculateBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!height.getText().toString().isEmpty() && !weight.getText().toString().isEmpty()) {
+            hideKeyboard(this);
+            calculateBMI();
+        } else {
+            hideKeyboard(this);
+            Toast.makeText(BmiActivity.this, "Please Enter All Info..", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void calculateBMI() {
+        double weight1 = Double.parseDouble(weight.getText().toString());
+        weight1 *= 2.205;
+        double height1 = Double.parseDouble(height.getText().toString());
+        double result = (703 * weight1 / (height1 * height1));
+        bmiTV.setText("your BMI: " + String.format("%.2f", result));
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+
+        View view = activity.getCurrentFocus();
+
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
 
