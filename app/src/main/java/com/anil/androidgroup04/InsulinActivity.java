c5f7c1fa43e4c7022acb0eca671a4b5f16f84d16
Chaitanya Swaroop <chaitanya.swaroop129@gmail.com>
2022-04-29 18:43:28 -0500

    insulin layout updated

--- app/src/main/java/com/anil/androidgroup04/InsulinActivity.java
+++ app/src/main/java/com/anil/androidgroup04/InsulinActivity.java
@@ -1,7 +1,6 @@
 package com.anil.androidgroup04;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -29,21 +28,13 @@ public class InsulinActivity extends AppCompatActivity {
     }
     public void initViews(){
 
-        tvFirstDose = findViewById(R.id.tv_first_dose);
-        tvSecondDose = findViewById(R.id.tv_seconddose);
+        tvFirstDose = findViewById(R.id.tv_first_insulin);
 
-        rgFirstDose = findViewById(R.id.rg_firstdose);
-        rgSecondDose = findViewById(R.id.rg_seconddose);
+        rgFirstDose = findViewById(R.id.rg_firstinsulin);
 
-        tilFirstDose = findViewById(R.id.til_firstdose);
-        tilSecondDose = findViewById(R.id.til_seconddose);
 
-        tvNextDosageHeader = findViewById(R.id.tv_next_dosage_header);
-        tvNextDosageDate = findViewById(R.id.tv_next_dosage_date);
-
-        tieFirstDose = findViewById(R.id.tie_firstdose);
-
-        tieSecondDose = findViewById(R.id.tie_second_dose);
+        tvNextDosageHeader = findViewById(R.id.tv_next_insulin_header);
+        tvNextDosageDate = findViewById(R.id.tv_next_insulin_date);
 
     }
 }
