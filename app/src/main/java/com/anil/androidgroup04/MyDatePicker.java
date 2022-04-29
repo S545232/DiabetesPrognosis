de09e9af00606301cb481048ac571a9fad6c0137
Chaitanya Swaroop <chaitanya.swaroop129@gmail.com>
2022-04-29 18:11:22 -0500

    custom date picker logic added

--- app/src/main/java/com/anil/androidgroup04/MyDatePicker.java
+++ app/src/main/java/com/anil/androidgroup04/MyDatePicker.java
@@ -1,9 +1,40 @@
 package com.anil.androidgroup04;
 
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MyDatePicker extends DialogFragment {
 
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        Calendar mCalendar = Calendar.getInstance();
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+        int dayOfMonth = mCalendar.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog mDatePickerDialogFragment = null;
 
+        try {
+            mDatePickerDialogFragment = new DatePickerDialog(getActivity(), (DatePickerDialog.OnDateSetListener)
+                    getActivity(), year, month, dayOfMonth);
+            String myDate = "2021/03/01 18:10:45";
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+            Date date = sdf.parse(myDate);
+            long millis = date.getTime();
+            mDatePickerDialogFragment.getDatePicker().setMinDate(millis);
+        } catch (Exception e) {
+
+        }
+        return mDatePickerDialogFragment;
+    }
 }
