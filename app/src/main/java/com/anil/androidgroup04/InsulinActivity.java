0b3d93ce99e67483834eed88b5a42faefec1a9e0
Chaitanya Swaroop <chaitanya.swaroop129@gmail.com>
2022-04-29 18:03:51 -0500

    insulin layout views initialized

--- app/src/main/java/com/anil/androidgroup04/InsulinActivity.java
+++ app/src/main/java/com/anil/androidgroup04/InsulinActivity.java
@@ -1,9 +1,24 @@
 package com.anil.androidgroup04;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
 public class InsulinActivity extends AppCompatActivity {
+    TextView displayTV, tvFirstDose, tvSecondDose, tvNextDosageHeader, tvNextDosageDate;
+
+    RadioGroup rgFirstDose, rgSecondDose;
+    RadioButton rbModernaVaccine, rbJJVaccine;
+
+    TextInputLayout tilFirstDose, tilSecondDose;
+    TextInputEditText tieFirstDose, tieSecondDose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +26,24 @@ public class InsulinActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insulin);
 
         getSupportActionBar().setTitle("Insulin Injections");
+    }
+    public void initViews(){
+
+        tvFirstDose = findViewById(R.id.tv_first_dose);
+        tvSecondDose = findViewById(R.id.tv_seconddose);
+
+        rgFirstDose = findViewById(R.id.rg_firstdose);
+        rgSecondDose = findViewById(R.id.rg_seconddose);
+
+        tilFirstDose = findViewById(R.id.til_firstdose);
+        tilSecondDose = findViewById(R.id.til_seconddose);
+
+        tvNextDosageHeader = findViewById(R.id.tv_next_dosage_header);
+        tvNextDosageDate = findViewById(R.id.tv_next_dosage_date);
+
+        tieFirstDose = findViewById(R.id.tie_firstdose);
+
+        tieSecondDose = findViewById(R.id.tie_second_dose);
+
     }
 }
